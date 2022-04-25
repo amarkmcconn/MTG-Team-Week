499f33db8296743ef5f29b6f0219ccce89ca83c2
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-25 14:01:54 -0700

    continue UI and API build out
    Co-authored-by: Nick Benzinger <Bennyz2030@users.noreply.github.com>
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 import GetCard from './js/card.js';
-import GetEnchantments from './js/get-enchantment-api.js';
-import GetInstants from './js/get-instant-api.js';
+// import GetEnchantments from './js/get-enchantment-api.js';
+// import GetInstants from './js/get-instant-api.js';
+// import GetCreature from './js/get-creature-api.js';
+// import GetLand from './js/get-land-api.js';
+// import GetSorceries from './js/get-sorc-api.js';
+
 
 
 $(document).ready(function() {
