3c260b3b1d15bfacb1ee16673f7cefc0d3bd6d4e
Mark McConnell <amark.mcconnell@gmail.com>
2022-04-27 11:44:08 -0700

    udpate api call.
    Co-authored-by: Meron G. Tekie <MeronTekie@users.noreply.github.com>
    Co-authored-by: Jake Edgar <jakeedgar@users.noreply.github.com>
    Co-authored-by: Nick Benzinger <Bennyz2030@users.noreply.github.com>

--- src/js/trainers-api.js
+++ src/js/trainers-api.js
@@ -1,6 +1,7 @@
 export default class GetTrainers {  
   static getCard() {
-    return fetch(`https://api.pokemontcg.io/v2/cards?supertype=Trainer`)
+    let page = 41;
+    return fetch(`https://api.pokemontcg.io/v2/cards/?page=${page}&${process.env.API_KEY}/`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.status);
@@ -8,7 +9,7 @@ export default class GetTrainers {
         return response.json();
       })
       .catch(function(error) {
-        return Error(error);
+        return (error);
       });
   }
 }
